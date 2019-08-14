b078910ce32698b42ea500e5545e2ab1a4e9dccc
Zacck Osiemo <coderv63@gmail.com>
2019-08-14 10:51:39 +0200

    Log whole model rather than non existent key

--- src/Home/View.elm
+++ src/Home/View.elm
@@ -11,7 +11,7 @@ import RemoteData exposing (..)
 view : Model -> Element Msg
 view model =
   let
-      _= Debug.log "You got session" model.session
+      _= Debug.log "You got session" model
   in
 
     column [ spacing 5 ]
