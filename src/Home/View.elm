0cb20f00625efa02297bfa15a48a82a93d6d7de4
dominicsantoSA <dsanto03@gmail.com>
2019-08-14 10:57:02 +0200

    remove: uneeded debug log

--- src/Home/View.elm
+++ src/Home/View.elm
@@ -10,9 +10,5 @@ import RemoteData exposing (..)
 
 view : Model -> Element Msg
 view model =
-  let
-      _= Debug.log "You got session" model.session
-  in
-
     column [ spacing 5 ]
         [ el [ heading 2 ] (text "Welcome to Arty") ]
